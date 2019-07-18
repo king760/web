fb6c8f56a2b6aa6c0cb8faea9b827f0abba887c0
laura <king760pr6204@gmail.com>
2019-07-18 18:29:46 -0400

    lo que hicimos

--- pendientes/views.py
+++ pendientes/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from pendientes.models import Tarea
+from random import randint
 # Create your views here.
 
 def index(request):
-    saludo = "Hola, Mundo! Esta es la raiz /"
-    return HttpResponse(saludo) #retornamos el saludo
+    listita = Tarea.objects.all()
+    persona = {
+        "nombre":"pedro",
+        "edad":"22",
+        "hobbies": "tomar terere",
+        "lista_tareas":listita,
+        }
+
+    # return HttpResponse(saludo) #retornamos el saludo
+    return render(request,"inicio.html", persona) #retornamos el saludo
+
+
+def tarea(request):
+    return HttpResponse("hola soy la vista tarea")
+
+#crear una vista respuestas que retorne un texto cuando en el navegador entremos http___-___--/info
+#lista crear la funcion /vista en viws y conectar a url usando path
+def pepe(request):
+    return HttpResponse("hola soy pepito")
+def nuevo(request):
+    return render(request, "inicio.html")
--- todolist/urls.py
+++ todolist/urls.py
@@ -20,5 +20,8 @@ from pendientes import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'), #Creamos la ruta raiz '' y la enlazamos con nuestra vista index del archivo views.py
+    path('', views.index, name='index'),
+    path("tareas", views.tarea),
+    path("pepe", views.pepe), #Creamos la ruta raiz '' y la enlazamos con nuestra vista index del archivo views.py
+    path("nuevo", views.nuevo),
 ]
